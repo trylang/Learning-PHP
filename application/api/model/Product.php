9792505ae8ebda223aee01683549da7f0df5656b
Jane <trylang@foxmail.com>
2017-12-12 20:53:56 +0800

    完善产品返回图片路径

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -14,4 +14,8 @@ class Product extends BaseModel
     protected $hidden = [
         'delete_time', 'main_img_id', 'pivot', 'from', 'category_id',
         'create_time', 'update_time'];
+
+    protected function getMainImgUrlAttr($value, $data){
+        return $this->prefixImgUrl($value, $data);
+    }
 }
